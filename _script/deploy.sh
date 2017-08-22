5fd7b1485c31e17c9e10c03f6c53d0844a9778e6
Phil Schanely <pschanely@miletwo.us>
2017-08-21 23:03:34 -0400

    Patch travis file.

--- _script/deploy.sh
+++ _script/deploy.sh
@@ -3,11 +3,11 @@
 if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
 then
     ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
-    rm -rf web1/site
-    mkdir web1/site
+    rm -rf scripting/site
+    mkdir scripting/site
     quit
 EOF
 
     cd _site || exit
-    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /scriptin/site .
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /scripting/site .
 fi
